c34e36dbeb37e57224d352c7c712abed33362fa3
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2020-10-11 18:25:10 -0300

    Adjusted input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,17 @@
 // eslint-disable-next-line no-use-before-define
 import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
 
 import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon?: React.ComponentType;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
 const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
   <Container>
-    {Icon && <Icon />}
+    {Icon && <Icon size={20} />}
     <input {...rest} />
   </Container>
 );
