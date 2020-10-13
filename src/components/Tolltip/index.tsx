637fd55cd40425b30f79876b2ad39b4cbafac27e
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2020-10-13 19:18:34 -0300

    Add tootip to project and input

---
+++ src/components/Tolltip/index.tsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-use-before-define
+import React from 'react';
+
+import { Container } from './styles';
+
+interface TooltipProps {
+  title?: string;
+  className?: string;
+}
+
+const Tolltip: React.FC<TooltipProps> = ({ children, className, title }) => (
+  <Container className={className}>
+    {children}
+    <span>{title}</span>
+  </Container>
+);
+
+export default Tolltip;
