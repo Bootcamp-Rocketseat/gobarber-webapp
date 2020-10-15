4d69914beba153958141f618960cb36bea8f878f
Maykon A. Oliveira <maykon.oliveira@neoway.com.br>
2020-10-15 17:37:21 -0300

    Added animation to toast from/leave

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTransition } from 'react-spring';
 
 import { Container } from './styles';
 
@@ -11,10 +12,16 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ toasts }) => {
+  const toastsWithTransitions = useTransition(toasts, toast => toast.id, {
+    from: { right: '-120%', opacity: 0 },
+    enter: { right: '0%', opacity: 1 },
+    leave: { right: '-120%', opacity: 0 },
+  });
+
   return (
     <Container>
-      {toasts.map(toast => (
-        <Toast key={toast.id} toast={toast} />
+      {toastsWithTransitions.map(({ item, key, props }) => (
+        <Toast key={key} toast={item} style={props} />
       ))}
     </Container>
   );
